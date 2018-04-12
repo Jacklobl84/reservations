7dcd6e60abee5a57830f67960896765e369948a4
jake <38048514+Jacklobl84@users.noreply.github.com>
2018-04-12 14:42:31 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+    'Bob': { claimed: false },
+    'ted': { claimed: true }
 }
-
 var name = prompt('Please enter the name for your reservation');
-
+name = name.toLowerCase();
 var claimReservation = function () {
-  // write your code here!
+    if (reservations[name]) {
+        if (reservations[name]["claimed"]) {
+            alert('I am sorry ' + name + ', your reservation is already claimed');
+        } else {
+            alert('Welcome ' + name);
+        }
+    } else {
+       reservations [name] = { claimed: "true" };
+       alert(name + ' you now have a reservation');
+   }
 }
-
 claimReservation();
